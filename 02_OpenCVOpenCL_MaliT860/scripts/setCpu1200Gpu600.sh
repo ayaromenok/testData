e1464d4a432ee3c50ba47a8e409ae48ab596a31f
Andrey E. Yaromenok <ayaromenok@gmail.com>
2018-12-31 16:51:45 +0100

    #3 updates to cpu/gpu scripts

--- 02_OpenCVOpenCL_MaliT860/scripts/setCpu1200Gpu600.sh
+++ 02_OpenCVOpenCL_MaliT860/scripts/setCpu1200Gpu600.sh
@@ -1,11 +1,10 @@
-sudo su
 echo "CPU-A53 - set to 1200 MHz"
-echo "performance" > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
-echo 1200000 > /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq
+sudo echo "performance" > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
+sudo echo 1200000 > /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq
 echo "CPU-A72 - set to 1200 MHz"
-echo "performance" > /sys/devices/system/cpu/cpufreq/policy4/scaling_governor
-echo 1200000 > /sys/devices/system/cpu/cpufreq/policy4/scaling_max_freq
+sudo echo "performance" > /sys/devices/system/cpu/cpufreq/policy4/scaling_governor
+sudo echo 1200000 > /sys/devices/system/cpu/cpufreq/policy4/scaling_max_freq
 echo "GPU-Mali - set to 600 MHz"
-echo "performance" > /sys/class/misc/mali0/device/devfreq/ff9a0000.gpu/governor
-echo 600000000 > /sys/class/misc/mali0/device/devfreq/ff9a0000.gpu/max_freq
-exit
+sudo echo "performance" > /sys/class/misc/mali0/device/devfreq/ff9a0000.gpu/governor
+sudo echo 600000000 > /sys/class/misc/mali0/device/devfreq/ff9a0000.gpu/max_freq
+echo "done"
